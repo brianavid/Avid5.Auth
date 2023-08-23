492c790e88d869439caf56c00e26be87dedaba49
brianavid <collins.bj@gmail.com>
2023-08-23 17:35:12 +0100

    More commenting

--- Avid5.Auth/Controllers/AuthController.cs
+++ Avid5.Auth/Controllers/AuthController.cs
@@ -7,42 +7,7 @@ using System.Net.Http.Headers;
 
 namespace Avid5.Auth.Controllers
 {
-    public class Token
-    {
-        [JsonProperty("access_token")]
-        public String AccessToken { get; set; }
-
-        [JsonProperty("token_type")]
-        public String TokenType { get; set; }
-
-        [JsonProperty("expires_in")]
-        public int ExpiresIn { get; set; }
-
-        [JsonProperty("refresh_token")]
-        public String RefreshToken { get; set; }
-
-        [JsonProperty("error")]
-        public String Error { get; set; }
-        [JsonProperty("error_description")]
-        public String ErrorDescription { get; set; }
-
-        public DateTime CreateDate { get; set; }
-        public Token()
-        {
-            AccessToken = "";
-            TokenType = "";
-            RefreshToken = "";
-            Error = "";
-            ErrorDescription = "";
-            CreateDate = DateTime.Now;
-        }
-        public Boolean IsExpired()
-        {
-            return CreateDate.Add(TimeSpan.FromSeconds(ExpiresIn)) >= DateTime.Now;
-        }
-    }
-
-    //  To use this code in another project, you will need three pieces of data.
+    //  To use this OAUTH code in another project, you will need three pieces of data.
     //  One is the public URL on which this long-lived service will be running.
     //  The other two are a ClientID and ClientSecret obrained from the Spotify
     //  developer management website.
@@ -54,7 +19,7 @@ namespace Avid5.Auth.Controllers
         private const string ClientId = "7f47df26bc5f4d79abafc0b8396fe208";
         //  Your client Secret. Replace with real secret before deploying
         private const string ClientSecret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-        //  The authentication service public URL which has been regostered with Spotify
+        //  The authentication service public URL which has been registered with Spotify
         private const string RedirectUri = "http://brianavid.dnsalias.com:88/Auth";
 
         static Logger logger = LogManager.GetCurrentClassLogger();
@@ -103,6 +68,7 @@ namespace Avid5.Auth.Controllers
                 String responseData = "";
                 try
                 {
+                    //  Send the data to Spotify for processing and authentication
                     using (var request = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token"))
                     {
                         request.Headers.CacheControl = new CacheControlHeaderValue { NoCache = true };
@@ -122,11 +88,15 @@ namespace Avid5.Auth.Controllers
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
                 }
 
+                //  The response from Spotify is a JSON-encoded structure
                 logger.Info("Response JSON {0}", responseData);
 
                 var token = JsonConvert.DeserializeObject<Token>(responseData);
                 lastRefreshToken = token?.RefreshToken ?? "";
+
+                //  The caller has two minutes to fetch the token after Authentication before it is forgotten here
                 refreshTokenFetchExpiry = DateTime.UtcNow.AddSeconds(120);
+
                 logger.Info("Last Refresh Token {0}", lastRefreshToken);
                 logger.Info("Fetch Refresh Token before {0}", refreshTokenFetchExpiry.ToShortTimeString());
                 logger.Info("New Token {0}", token?.AccessToken ?? "");
@@ -158,10 +128,10 @@ namespace Avid5.Auth.Controllers
             return this.Content("OK");
         }
 
-        //  Auth/Authenticate is used to initially authenticate using SpotifyAPI.Web.LoginRequest(),
-        //  starting with just the ClientId.
-        //  This will cause the client to display a browser, which (unless they are locally cached) may
-        //  need credentials to complete the login.
+        //  Auth/Authenticate is used to initially authenticate using a call to
+        //  SpotifyAPI.Web.LoginRequest(), starting with just the ClientId.
+        //  LoginRequest() will return to the caller a handshake URL to display a browser, which
+        //  (unless they are already locally cached) may need entry of credentials to complete the login.
         public ContentResult Authenticate(
             string code,
             string error,
@@ -174,6 +144,7 @@ namespace Avid5.Auth.Controllers
         //  Having authenticated, Auth/GetLastRefreshToken will return a "RefreshToken" that can be used thereafter
         //  to authenticate future sesions without any UI.
         //  The client MUST save this token for any future authenticaton without UI via Auth/Refresh
+        //  The caller has two minutes to fetch the token after Authentication before it is forgotten here
         public ContentResult GetLastRefreshToken()
         {
             if (DateTime.UtcNow > refreshTokenFetchExpiry)
@@ -184,9 +155,9 @@ namespace Avid5.Auth.Controllers
             return this.Content(lastRefreshToken);
         }
 
-        //  Authhentication will grant use of Spotify for a short period. Before that period expires,
+        //  Authentication will grant use of Spotify for a short period. Before that period expires,
         //  Auth/Refresh must be called to allocate a new RefreshToken with more lifetime.
-        //  See class Token above for the structure returned by Spotify encoded as JSON.
+        //  See class Token below for the structure returned by Spotify encoded as JSON.
         public ContentResult Refresh(
             string refresh_token)
         {
@@ -195,5 +166,41 @@ namespace Avid5.Auth.Controllers
         }
 
     }
+
+    public class Token
+    {
+        [JsonProperty("access_token")]
+        public String AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public String TokenType { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public String RefreshToken { get; set; }
+
+        [JsonProperty("error")]
+        public String Error { get; set; }
+        [JsonProperty("error_description")]
+        public String ErrorDescription { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public Token()
+        {
+            AccessToken = "";
+            TokenType = "";
+            RefreshToken = "";
+            Error = "";
+            ErrorDescription = "";
+            CreateDate = DateTime.Now;
+        }
+        public Boolean IsExpired()
+        {
+            return CreateDate.Add(TimeSpan.FromSeconds(ExpiresIn)) >= DateTime.Now;
+        }
+    }
+
 }
 
